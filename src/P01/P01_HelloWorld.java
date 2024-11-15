9e4f1e198111c1f4cc8c40fdb9e97cfe94683cd3
neslihansonmezz <nes1992.ns@gmail.com>
2024-11-15 09:40:20 +0100

    first commit

---
+++ src/P01/P01_HelloWorld.java
@@ -0,0 +1,11 @@
+package P01;
+
+public class P01_HelloWorld {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/P02/P02.java
@@ -0,0 +1,10 @@
+package P02;
+
+public class P02 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("merhaba github");
+    }
+}
